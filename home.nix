e59216df0ac994591d121ad5d75e99c336b5f71b
Кунгуров Макар Евгеньевич <mkungurov@runity.ru>
2024-03-29 18:26:47 +0300

    Config update

--- home.nix
+++ home.nix
@@ -441,7 +441,7 @@ in {
               detach-client
             } {
               display-popup -h 70% -w 70% -E -d "#{pane_current_path}" -T "#{pane_current_path}" tmux new -A -s shell -n "shell"
-              send-keys cd "#{pane_current_path}"
+              send-keys -t "shell" cd "#{pane_current_path}"
             }
             bind-key g display-popup -h 80% -w 80% -E -d "#{pane_current_path}" -T "#{pane_current_path}" tmux new -s lazygit -n lazygit "${
         lib.getExe pkgs.lazygit
