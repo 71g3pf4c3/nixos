f09f71261408572db89b45e8e3e3d9a387613662
Кунгуров Макар Евгеньевич <mkungurov@runity.ru>
2024-03-29 19:23:07 +0300

    Config update

--- home.nix
+++ home.nix
@@ -440,7 +440,7 @@ in {
             bind-key b if-shell -F '#{==:#{session_name},scratch}' {
               detach-client
             } {
-              display-popup -h 70% -w 70% -E -d "#{pane_current_path}" -T "#{pane_current_path}" tmux new -A -s scratch -n "scratch -c $PWD"
+              display-popup -h 70% -w 70% -E -d "#{pane_current_path}" -T "#{pane_current_path}" tmux new -A -s scratch -n "scratch" -c $PWD
             }
             bind-key g display-popup -h 80% -w 80% -E -d "#{pane_current_path}" -T "#{pane_current_path}" tmux new -s lazygit -n lazygit "${
         lib.getExe pkgs.lazygit
