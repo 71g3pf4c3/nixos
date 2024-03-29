124c7d10744af737ee9d46dace7e0aaac2540b26
Кунгуров Макар Евгеньевич <mkungurov@runity.ru>
2024-03-29 11:39:27 +0300

    Config update

--- nixvim.nix
+++ nixvim.nix
@@ -39,29 +39,38 @@
       which-key.enable = true;
       lualine.enable = true;
       transparent.enable = true;
-      cmp.enable = true;
-      cmp.extraOptions = {
-        mapping = {
-          "<C-Space>" = "cmp.mapping.complete()";
-          "<C-d>" = "cmp.mapping.scroll_docs(-4)";
-          "<C-e>" = "cmp.mapping.close()";
-          "<C-f>" = "cmp.mapping.scroll_docs(4)";
-          "<CR>" = "cmp.mapping.confirm({ select = true })";
-          "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
-          "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+      cmp = {
+        enable = true;
+        autoEnableSources = true;
+        settings.allow_fullfuzzy_matching = true;
+        settings = {
+          mapping = {
+            "<C-Space>" = "cmp.mapping.complete()";
+            "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+            "<C-e>" = "cmp.mapping.close()";
+            "<C-f>" = "cmp.mapping.scroll_docs(4)";
+            "<CR>" = "cmp.mapping.confirm({ select = true })";
+            "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+            "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+          };
+        };
+        cmdline = {
+          "/" = {
+            mapping = {__raw = "cmp.mapping.preset.cmdline()";};
+            sources = [{name = "buffer";}];
+          };
+          ":" = {
+            mapping = {__raw = "cmp.mapping.preset.cmdline()";};
+            sources = [
+              {name = "path";}
+              {
+                name = "cmdline";
+                option = {ignore_cmds = ["Man" "!"];};
+              }
+            ];
+          };
         };
       };
-      cmp.settings.sources = [
-        {name = "tmux";}
-        {name = "buffer";}
-        {name = "cmdline";}
-        {name = "dictionary";}
-        {name = "emoji";}
-        {name = "path";}
-        {name = "treesitter";}
-        {name = "luasnip";}
-        {name = "nvim_lsp";}
-      ];
       twilight.enable = true;
       cmp-tmux.enable = true;
       cmp-git.enable = true;
