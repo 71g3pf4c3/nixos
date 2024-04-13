e4755fc9b82c3daea86ddbe3f93d705c374b81e4
71g3pf4c3 <71g3pf4c3@gmail.com>
2024-04-13 17:23:25 +0300

    Add additional colors

--- home/colors/default.nix
+++ home/colors/default.nix
@@ -1,25 +1,63 @@
-{ config, pkgs, unstable, lib, inputs, ... }: {
+{
+  config,
+  pkgs,
+  unstable,
+  lib,
+  inputs,
+  ...
+}:
+{
   colorScheme = {
     slug = "pasque";
     name = "Pasque";
     author = "Gabriel Fontes (https://github.com/Misterio77)";
     palette = {
-      base00 = "#271C3A";
-      base01 = "#100323";
-      base02 = "#3E2D5C";
-      base03 = "#5D5766";
-      base04 = "#BEBCBF";
-      base05 = "#DEDCDF";
-      base06 = "#EDEAEF";
-      base07 = "#BBAADD";
-      base08 = "#A92258";
-      base09 = "#918889";
-      base0A = "#804ead";
-      base0B = "#C6914B";
-      base0C = "#7263AA";
-      base0D = "#8E7DC6";
-      base0E = "#953B9D";
-      base0F = "#59325C";
+      base00 = "fbf1c7";
+      base01 = "ebdbb2";
+      base02 = "d5c4a1";
+      base03 = "bdae93";
+      base04 = "665c54";
+      base05 = "504945";
+      base06 = "3c3836";
+      base07 = "282828";
+      base08 = "9d0006";
+      base09 = "af3a03";
+      base0A = "b57614";
+      base0B = "79740e";
+      base0C = "427b58";
+      base0D = "076678";
+      base0E = "8f3f71";
+      base0F = "d65d0e";
+      bg = "ebdbb2";
+      red = "cc241d";
+      green = "98971a";
+      yellow = "d79921";
+      blue = "458588";
+      purple = "b16286";
+      aqua = "689d6a";
+      gray = "7c6f64";
+      redalt = "9d0006";
+      greenalt = "79740e";
+      yellowalt = "b57614";
+      bluealt = "076678";
+      magentaalt = "8f3f71";
+      aquaalt = "427b58";
+      fg = "3c3836";
+      grayalt = "928374";
+      bg0_h = "f9f5d7";
+      bg0 = "fbf1c7";
+      bg1 = "ebdbb2";
+      bg2 = "d5c4a1";
+      bg3 = "bdae93";
+      bg4 = "a89984";
+      orange = "d65d0e";
+      bg0_s = "f2e5bc";
+      fg0 = "282828";
+      fg1 = "3c3836";
+      fg2 = "504945";
+      fg3 = "665c54";
+      fg4 = "7c6f64";
+      orangealt = "af3a03";
     };
   };
 }
