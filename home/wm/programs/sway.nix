85aaad8ab5a8c371e9176a9fb6f8be2ab0f61583
71g3pf4c3 <71g3pf4c3@gmail.com>
2024-04-24 15:43:13 +0300

    Sus

--- home/wm/programs/sway.nix
+++ home/wm/programs/sway.nix
@@ -26,8 +26,9 @@ in
     extraConfig = ''
          titlebar_border_thickness 0
          titlebar_padding 20 0
-      exec dbus-sway-environment
-      exec configure-gtk
+         exec dbus-sway-environment
+         exec configure-gtk
+         for_window [app_id="waydroid.org.*"] floating enable
     '';
     config = {
       startup = [
