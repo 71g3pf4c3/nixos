cd83dec93e2910324971527ca4e68edb77ec14bf
71g3pf4c3 <71g3pf4c3@gmail.com>
2025-05-16 18:45:16 +0300

    Update from Fri May 16 06:45:16 PM MSK 2025

--- home/terminal/programs/kubernetes/default.nix
+++ home/terminal/programs/kubernetes/default.nix
@@ -24,5 +24,32 @@
     kubernetes-helm
     kubevirt
     velero
+
+    argocd
+    cilium-cli
+    clusterctl
+    cmctl
+    distrobox
+    helm-docs
+    hubble
+    # k8sgpt
+    kind
+    kube-capacity
+    kubectl
+    kubectl-cnpg
+    kubectl-node-shell
+    kubectl-tree
+    kubectl-validate
+    kubectl-neat
+    kubectl-images
+    kubectl-example
+    kubectx
+    kubeconform
+    kubelogin-oidc
+    kubernetes-helm
+    kubeseal
+    kubevirt
+    # packer
+    popeye
   ];
 }
